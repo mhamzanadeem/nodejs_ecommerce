41d8770aa98a271b8268ac03bb5d69036948f530
Hamza Nadeem <nadeemmhamza@gmail.com>
2026-07-13 19:19:50 +0500

    local mongodb connected

---
+++ backend/config/database.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose")
+
+const connectDatabase = () => { mongoose.connect(process.env.DB_URI).then((data) => { console.log(`Mongodb connect with server: ${data.connection.host}`) }).catch((err) => { console.log(err) }) }
+
+
+module.exports = connectDatabase
